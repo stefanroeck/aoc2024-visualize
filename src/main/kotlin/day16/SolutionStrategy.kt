09f93aa13b6eb07944d950ba11afabd71fa5da81
Stefan Roeck <stefan.roeck@gmail.com>
2025-01-23 22:38:25 +0100

    Add left and right hand wall strategy

--- src/main/kotlin/day16/SolutionStrategy.kt
+++ src/main/kotlin/day16/SolutionStrategy.kt
@@ -43,35 +43,46 @@ private class LongestDistanceToTarget : DirectionStrategy {
     }
 }
 
-private class HandOnWall : DirectionStrategy {
-    val sortedDirections = listOf(Direction.Up, Direction.Right, Direction.Down, Direction.Left)
+enum class HandDirection {
+    Left, Right,
+}
 
-    private fun Direction.turnRight() = sortedDirections[(sortedDirections.indexOf(this) + 1).mod(4)]
-    private fun Direction.turnLeft() = sortedDirections[(sortedDirections.indexOf(this) - 1).mod(4)]
-    private fun Direction.turnBack() = sortedDirections[(sortedDirections.indexOf(this) + 2).mod(4)]
+private class HandOnWall(private val handDirection: HandDirection) : DirectionStrategy {
+    companion object {
+        private val sortedDirections = listOf(Direction.Up, Direction.Right, Direction.Down, Direction.Left)
+        private fun Direction.turnRight() = sortedDirections[(sortedDirections.indexOf(this) + 1).mod(4)]
+        private fun Direction.turnLeft() = sortedDirections[(sortedDirections.indexOf(this) - 1).mod(4)]
+    }
 
     override fun sortPossibleDirections(
         candidates: List<Triple<Direction, Point, Costs>>,
         currentDirection: Direction,
         endPosition: Point
     ): List<Triple<Direction, Point, Costs>> {
-        // preferred order: right, straight, left, back
-        val preferredDirections = listOf(
-            currentDirection.turnRight(),
-            currentDirection,
-            currentDirection.turnLeft(),
-            currentDirection.turnBack(),
-        )
+        val preferredDirections = when (handDirection) {
+            HandDirection.Left -> listOf(
+                currentDirection.turnLeft(),
+                currentDirection,
+                currentDirection.turnRight(),
+            )
+
+            HandDirection.Right -> listOf(
+                currentDirection.turnRight(),
+                currentDirection,
+                currentDirection.turnLeft(),
+            )
+
+        }
         return preferredDirections.mapNotNull { pref ->
             candidates.firstOrNull { it.first == pref }
         }
     }
-
 }
 
 enum class SolutionStrategy(val fn: DirectionStrategy, val label: String) {
     LowestCost(LowestCostDirectionStrategy(), "Lowest Costs"),
     ShortestDistance(ShortestDistanceToTarget(), "Shortest Distance"),
     LongestDistance(LongestDistanceToTarget(), "Longest Distance"),
-    HandOnWall(HandOnWall(), "Hand on Wall")
+    HandOnLeftWall(HandOnWall(HandDirection.Left), "Hand on left Wall"),
+    HandOnRightWall(HandOnWall(HandDirection.Right), "Hand on right Wall"),
 }
